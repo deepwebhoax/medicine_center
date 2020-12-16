2721208ce67cb7af0e3efc942430f775b2cb9758
deepwebhoax <32373109+deepwebhoax@users.noreply.github.com>
2020-12-17 01:28:22 +0200

    bug fixes

--- app/validators/schemes/appointment_scheme.py
+++ app/validators/schemes/appointment_scheme.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel, validator, ValidationError, root_validator
 from datetime import datetime
-import pytz
 from typing import Optional
 from app.api.repository import Repository
 
@@ -19,7 +18,7 @@ class AppointmentScheme(BaseModel):
                         for s in schedule if s['weekDay']==v.strftime('%A')] # get working hours on the appointment day
         v = v.replace(tzinfo=None)
         for start, finish in workingHours:
-            if start < v < finish:
+            if start.time() <= v.time() <= finish.time():
                 return v
         raise ValueError(f'Out of schedule! Doctor is working today at {workingHours}')
         
@@ -28,11 +27,9 @@ class AppointmentScheme(BaseModel):
     # @validator('finishDateTime')
     @root_validator()
     def is_unoccupied(cls, values):
-        if 'startDateTime' in values:
-            print(values)
+        if 'startDateTime' in values and 'finishDateTime' in values:
             values['startDateTime'] = values['startDateTime'].replace(tzinfo=None)
             values['finishDateTime'] = values['finishDateTime'].replace(tzinfo=None)
-            print(values)
             doctors_appointments = Repository.get_appointments_by_doctor(values['doctor_id'])
             for a in doctors_appointments:
                 if a['startDateTime'] <= values['startDateTime'] <= a['finishDateTime']  or a['startDateTime'] <= values['finishDateTime'] <= a['finishDateTime']:
@@ -50,7 +47,7 @@ class AppointmentScheme(BaseModel):
     # @validator('startDateTime', 'finishDateTime')
     @root_validator()
     def is_short(cls, values):
-        if 'startDateTime' in values:
+        if 'startDateTime' in values and 'finishDateTime' in values:
             if values['finishDateTime'].timestamp() - values['startDateTime'].timestamp() > 2*60*60:
                 raise ValueError('Appointments longer then 2 hours are not allowed')
 
